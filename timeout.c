fd647aca196af5afb823296c9c313cc54f7be80a
Valentin Hilbig <webmaster@scylla-charybdis.com>
2008-11-02 00:13:56 +0000

    First version

--- timeout.c
+++ timeout.c
@@ -3,68 +3,101 @@
  * Execute a program only a certain time.
  * Rewrite of a program which is over 10 years old.
  *
- * Copyright (C)2006 Valentin Hilbig, webmaster@scylla-charybdis.com
- * This shall be independent of tinolib.
+ * Copyright (C)2008 Valentin Hilbig, webmaster@scylla-charybdis.com
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  *
  * $Log$
- * Revision 1.1  2008-07-01 18:55:08  tino
- * save
+ * Revision 1.2  2008-11-02 00:13:56  tino
+ * First version
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
-#include <stdarg.h>
-#include <ctype.h>
-
-#include <unistd.h>
-
-#include <sys/ptrace.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "tino/alarm.h"
+#include "tino/proc.h"
 
 #define	MAX_SIG	100
 static struct signals
   {
     int		sig;
     long	delay;
-  }	maxsig[MAX_SIG];
+  }			sig[MAX_SIG];
 static int		sigs;
 
 static int		mypid;
 static const char	*arg0;
+static unsigned long	timeout;
+static unsigned long	seconds;
+static long		mark;
+static int		verbose;
 
 static void
-ex(int ret, const char *s, ...)
+vex(int ret, TINO_VA_LIST list, const char *type, int e)
 {
-  va_list	list;
-  int		e;
-
-  e	= errno;
-  fprintf(stderr, "[%ld] %s error: ", (long)mypid, arg0);
-  va_start(list, s);
-  vfprintf(stderr, s, list);
-  va_end(list);
-  fprintf(stderr, ": %s\n", strerror(e));
+  fprintf(stderr, "[%ld] %s %s: ", (long)mypid, arg0, type);
+  tino_vfprintf(stderr, list);
+  if (e)
+    fprintf(stderr, ": %s\n", strerror(e));
+  else
+    fprintf(stderr, "\n");
   exit(ret);
+}
+
+static void
+info(const char *s, ...)
+{
+  tino_va_list	list;
+
+  if (!verbose)
+    return;
+  fprintf(stderr, "[%ld] %s info: ", (long)mypid, arg0);
+  tino_va_start(list, s);
+  tino_vfprintf(stderr, &list);
+  tino_va_end(list);
+  fprintf(stderr, "\n");
+}
+
+static void
+ex(const char *s, ...)
+{
+  tino_va_list	list;
+
+  tino_va_start(list, s);
+  vex(210, &list, "error", errno);
+  /* never reached	*/
+}
+
+static void
+to(const char *s, ...)
+{
+  tino_va_list	list;
+
+  tino_va_start(list, s);
+  vex(211, &list, "timeout", 0);
+  /* never reached	*/
+}
+
+static void
+fin(int ret, const char *s, ...)
+{
+  tino_va_list	list;
+
+  tino_va_start(list, s);
+  vex(ret, &list, "info", 0);
+  /* never reached	*/
 }
 
 static void
@@ -78,29 +111,183 @@ setarg0(const char *s)
     arg0	= tmp+1;
 }
 
+static int
+cb(void *user, long delta, time_t now, long run)
+{
+  if (seconds==timeout)
+    mark++;
+  else
+    seconds++;
+  if (verbose>1)
+    {
+      fprintf(stderr, "%lu \r", timeout-seconds);
+      fflush(stderr);
+    }
+  return 0;
+}
+
+static void
+schedule(void)
+{
+  seconds	= 0;
+  mark		= 0;
+}
+
+static void
+check(const char *what)
+{
+  TINO_ALARM_RUN();
+  if (mark)
+    to("STDIN");
+}
+
+static void
+do_copy(int idle)
+{
+  for (;;)
+    {
+      char	buf[BUFSIZ];
+      int	got;
+      size_t	pos;
+
+      if ((got=tino_file_readI(0, buf, sizeof buf))==0)
+	return;
+
+      check("STDIN");
+
+      if (got<0)
+	{
+	  if (errno==EINTR)
+	    continue;
+	  ex("read STDIN");
+	}
+
+      if (!idle)
+	schedule();
+
+      for (pos=0; pos<got; )
+	{
+	  int	put;
+
+	  put	= tino_file_writeI(1, buf+pos, got-pos);
+
+	  check("STDOUT");
+
+	  if (put<0)
+	    {
+	      if (errno==EINTR)
+		continue;
+	      ex("write STDOUT");
+	    }
+
+	  if (!put)
+	    ex("zero write?");
+
+	  if (!idle)
+	    schedule();
+
+	  pos	+= put;
+	}
+    }
+}
+
+static void
+do_fork(char **argv)
+{
+  int		nr;
+  int		status, result;
+  pid_t		pid, tmp;
+  const char	*s;
+
+  pid	= tino_fork_exec(0, 1, 2, argv, NULL, 0, NULL);
+  info("[%ld] exec %s", (long)pid, argv[0]);
+  nr	= 0;
+  while ((tmp=waitpid((pid_t)-1, &status, 0))!=pid)
+    {
+      TINO_ALARM_RUN();
+      if (mark>0)
+	{
+	  if (nr>=sigs)
+	    to("out of signals");
+	  info("[%ld] send signal %d", (long)pid, sig[nr].sig);
+	  kill(pid, sig[nr].sig);
+	  mark	= 1l-sig[nr].delay;
+	  nr++;
+	}
+
+      if (pid==(pid_t)-1)
+	{
+	  if (errno==EINTR)
+	    continue;
+	  ex("wait error");
+	}
+    }
+  s	= tino_wait_child_status_string(status, &result);
+  if (verbose)
+    fin(result, "[%ld] result %s: %s", (long)pid, argv[0], s);
+  exit(result);
+}
+
 int
 main(int argc, char **argv)
 {
+  char	*end;
+
   setarg0(argv[0]);
-  for (sigs=0; argc>1 && **++argv=='-'; sigs++, argc--)
+  for (sigs=0; --argc>=1 && **++argv=='-'; )
     {
       unsigned long long	u, dly;
       char			*end;
 
-      u				= strtoull(*argv+1, &end, 0);
-      dly			= 1;
+      if (!strcmp(*argv, "-v"))
+	{
+	  verbose++;
+	  continue;
+	}
+      u			= strtoull(*argv+1, &end, 0);
+      dly		= 1;
       if (end && *end=='@')
 	{
 	  end++;
-	  dly			= strtoull(end+1, &end, 0);
+	  dly		= strtoull(end+1, &end, 0);
 	}
       if (u!=(unsigned long long)(int)u || dly!=(unsigned long long)(int)dly)
-	ex(-1, "overflow in number: %s", *argv);
+	ex("overflow in number: %s", *argv);
       if (!end || *end)
-	ex(-1, "invalid number: %s", *argv);
-      maxsig[sigs].sig		= u;
-      maxsig[sigs].delay	= dly;
+	ex("invalid option: %s", *argv);
+      sig[sigs].sig	= u;
+      sig[sigs].delay	= dly;
+      sigs++;
     }
-  000;
+  if (!sigs)
+    {
+      sig[sigs].sig	= 1;
+      sig[sigs].delay	= 1;
+      sigs++;
+      sig[sigs].sig	= 15;
+      sig[sigs].delay	= 1;
+      sigs++;
+      sig[sigs].sig	= 9;
+      sig[sigs].delay	= 1;
+      sigs++;
+    }
+
+  if (argc<1)
+    {
+      fprintf(stderr, "Usage: %s [-v] [-signal[@seconds]...] seconds [-|program [args...]]\n", arg0);
+      return 210;
+    }
+
+  timeout			= strtoull(*argv, &end, 0);
+  if (!end || *end)
+    ex("invalid seconds: %s", *argv);
+
+  tino_alarm_set(1, cb, NULL);
+  if (argc==2 && !strcmp(argv[1], "-"))
+    do_copy(1);
+  else if (argc==1)
+    do_copy(0);
+  else
+    do_fork(argv+1);
   return 0;
 }
